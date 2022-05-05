c2a3e978f020a952ae3ee3143663143938869d31
poornima.skp <poornima.skp@gmail.com>
2022-05-04 23:43:21 -0500

    Trying to Flip card in 3D

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 
-console.log('Let"s Play');
+// console.log('Let"s Play');
+
+const images = document.querySelectorAll('.game-image');
+
+// console.log(images);
+
+// for (let i = 0; i <= images.length; i++) {
+    // images[i].addEventListener('click', flipCard);
+// }
+
+// images.addEventListener('click', flipCard);
+
+images.forEach(card => card.addEventListener('click', flipCard));
+
+let hasFilppedCard = false;
+
+function flipCard() {
+    console.log('Clicked', this);
+    this.classList.add('filp');
+
+    if (!hasFilppedCard) {
+
+        hasFilppedCard = true;
+        let imageOne = this;
+
+        console.log(hasFilppedCard, imageOne)
+    }
+}
+
+
+
+
+
